5819accef4a055739b0eb6197ee2bbc7e1b58f3e
David C. Conner <david.conner@cnu.edu>
2019-06-21 18:24:31 -0400

    make code consistent with kinetic_devel on https://github.com/ros-planning/navigation_experimental; maintaining separate repo for now

--- src/sbpl_lattice_planner.cpp
+++ src/sbpl_lattice_planner.cpp
@@ -36,17 +36,25 @@
 *********************************************************************/
 
 #include <sbpl_lattice_planner/sbpl_lattice_planner.h>
-#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
 #include <nav_msgs/Path.h>
 #include <sbpl_lattice_planner/SBPLLatticePlannerStats.h>
 
 #include <costmap_2d/inflation_layer.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 using namespace std;
 using namespace ros;
 
 
-PLUGINLIB_DECLARE_CLASS(sbpl_latice_planner, SBPLLatticePlanner, sbpl_lattice_planner::SBPLLatticePlanner, nav_core::BaseGlobalPlanner);
+PLUGINLIB_EXPORT_CLASS(sbpl_lattice_planner::SBPLLatticePlanner, nav_core::BaseGlobalPlanner)
+
+namespace geometry_msgs {
+  bool operator== (const Point &p1, const Point &p2)
+  {
+    return p1.x == p2.x && p1.y == p2.y && p1.z == p2.z;
+  }
+}
 
 namespace sbpl_lattice_planner{
 
@@ -80,16 +88,15 @@ SBPLLatticePlanner::SBPLLatticePlanner()
   : initialized_(false), costmap_ros_(NULL){
 }
 
-SBPLLatticePlanner::SBPLLatticePlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+SBPLLatticePlanner::SBPLLatticePlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros) 
   : initialized_(false), costmap_ros_(NULL){
   initialize(name, costmap_ros);
 }
 
-
+    
 void SBPLLatticePlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
   if(!initialized_){
     ros::NodeHandle private_nh("~/"+name);
-    ros::NodeHandle nh(name);
 
     ROS_INFO("Name is %s", name.c_str());
 
@@ -112,9 +119,10 @@ void SBPLLatticePlanner::initialize(std::string name, costmap_2d::Costmap2DROS* 
     sbpl_cost_multiplier_ = (unsigned char) (costmap_2d::INSCRIBED_INFLATED_OBSTACLE/inscribed_inflated_obstacle_ + 1);
     ROS_DEBUG("SBPL: lethal: %uz, inscribed inflated: %uz, multiplier: %uz",lethal_obstacle,inscribed_inflated_obstacle_,sbpl_cost_multiplier_);
 
+    name_ = name;
     costmap_ros_ = costmap_ros;
 
-    std::vector<geometry_msgs::Point> footprint = costmap_ros_->getRobotFootprint();
+    footprint_ = costmap_ros_->getRobotFootprint();
 
     if ("XYThetaLattice" == environment_type_){
       ROS_DEBUG("Using a 3D costmap for theta lattice\n");
@@ -125,33 +133,34 @@ void SBPLLatticePlanner::initialize(std::string name, costmap_2d::Costmap2DROS* 
       exit(1);
     }
 
-    // check if the costmap has an inflation layer
-    // Warning: footprint updates after initialization are not supported here
-    unsigned char cost_possibly_circumscribed_tresh = 0;
-    for(std::vector<boost::shared_ptr<costmap_2d::Layer> >::const_iterator layer = costmap_ros_->getLayeredCostmap()->getPlugins()->begin();
-        layer != costmap_ros_->getLayeredCostmap()->getPlugins()->end();
-        ++layer) {
-      boost::shared_ptr<costmap_2d::InflationLayer> inflation_layer = boost::dynamic_pointer_cast<costmap_2d::InflationLayer>(*layer);
-      if (!inflation_layer) continue;
+    circumscribed_cost_ = computeCircumscribedCost();
 
-      cost_possibly_circumscribed_tresh = inflation_layer->computeCost(costmap_ros_->getLayeredCostmap()->getCircumscribedRadius());
+    if (circumscribed_cost_ == 0) {
+      // Unfortunately, the inflation_radius is not taken into account by
+      // inflation_layer->computeCost(). If inflation_radius is smaller than
+      // the circumscribed radius, SBPL will ignore some obstacles, but we
+      // cannot detect this problem. If the cost_scaling_factor is too large,
+      // SBPL won't run into obstacles, but will always perform an expensive
+      // footprint check, no matter how far the nearest obstacle is.
+      ROS_WARN("The costmap value at the robot's circumscribed radius (%f m) is 0.", costmap_ros_->getLayeredCostmap()->getCircumscribedRadius());
+      ROS_WARN("SBPL performance will suffer.");
+      ROS_WARN("Please decrease the costmap's cost_scaling_factor.");
     }
-
     if(!env_->SetEnvParameter("cost_inscribed_thresh",costMapCostToSBPLCost(costmap_2d::INSCRIBED_INFLATED_OBSTACLE))){
       ROS_ERROR("Failed to set cost_inscribed_thresh parameter");
       exit(1);
     }
-    if(!env_->SetEnvParameter("cost_possibly_circumscribed_thresh", costMapCostToSBPLCost(cost_possibly_circumscribed_tresh))){
+    if(!env_->SetEnvParameter("cost_possibly_circumscribed_thresh", circumscribed_cost_)){
       ROS_ERROR("Failed to set cost_possibly_circumscribed_thresh parameter");
       exit(1);
     }
     int obst_cost_thresh = costMapCostToSBPLCost(costmap_2d::LETHAL_OBSTACLE);
     vector<sbpl_2Dpt_t> perimeterptsV;
-    perimeterptsV.reserve(footprint.size());
-    for (size_t ii(0); ii < footprint.size(); ++ii) {
+    perimeterptsV.reserve(footprint_.size());
+    for (size_t ii(0); ii < footprint_.size(); ++ii) {
       sbpl_2Dpt_t pt;
-      pt.x = footprint[ii].x;
-      pt.y = footprint[ii].y;
+      pt.x = footprint_[ii].x;
+      pt.y = footprint_[ii].y;
       perimeterptsV.push_back(pt);
     }
 
@@ -166,9 +175,11 @@ void SBPLLatticePlanner::initialize(std::string name, costmap_2d::Costmap2DROS* 
                                 perimeterptsV, costmap_ros_->getCostmap()->getResolution(), nominalvel_mpersecs,
                                 timetoturn45degsinplace_secs, obst_cost_thresh,
                                 primitive_filename_.c_str());
+      current_env_width_ = costmap_ros_->getCostmap()->getSizeInCellsX();
+      current_env_height_ = costmap_ros_->getCostmap()->getSizeInCellsY();
     }
-    catch(SBPL_Exception e){
-      ROS_ERROR("SBPL encountered a fatal exception!");
+    catch(SBPL_Exception *e){
+      ROS_ERROR("SBPL encountered a fatal exception: %s", e->what());
       ret = false;
     }
     if(!ret){
@@ -195,11 +206,11 @@ void SBPLLatticePlanner::initialize(std::string name, costmap_2d::Costmap2DROS* 
     ROS_INFO("[sbpl_lattice_planner] Initialized successfully");
     plan_pub_ = private_nh.advertise<nav_msgs::Path>("plan", 1);
     stats_publisher_ = private_nh.advertise<sbpl_lattice_planner::SBPLLatticePlannerStats>("sbpl_lattice_planner_stats", 1);
-
+    
     initialized_ = true;
   }
 }
-
+  
 //Taken from Sachin's sbpl_cart_planner
 //This rescales the costmap according to a rosparam which sets the obstacle cost
 unsigned char SBPLLatticePlanner::costMapCostToSBPLCost(unsigned char newcost){
@@ -209,12 +220,16 @@ unsigned char SBPLLatticePlanner::costMapCostToSBPLCost(unsigned char newcost){
     return inscribed_inflated_obstacle_;
   else if(newcost == 0 || newcost == costmap_2d::NO_INFORMATION)
     return 0;
-  else
-    return (unsigned char) (newcost/sbpl_cost_multiplier_ + 0.5);
+  else {
+    unsigned char sbpl_cost = newcost / sbpl_cost_multiplier_;
+    if (sbpl_cost == 0)
+      sbpl_cost = 1;
+    return sbpl_cost;
+  }
 }
 
-void SBPLLatticePlanner::publishStats(int solution_cost, int solution_size,
-                                      const geometry_msgs::PoseStamped& start,
+void SBPLLatticePlanner::publishStats(int solution_cost, int solution_size, 
+                                      const geometry_msgs::PoseStamped& start, 
                                       const geometry_msgs::PoseStamped& goal){
   // Fill up statistics and publish
   sbpl_lattice_planner::SBPLLatticePlannerStats stats;
@@ -235,12 +250,58 @@ void SBPLLatticePlanner::publishStats(int solution_cost, int solution_size,
   stats_publisher_.publish(stats);
 }
 
+unsigned char SBPLLatticePlanner::computeCircumscribedCost() {
+  unsigned char result = 0;
+
+  if (!costmap_ros_) {
+    ROS_ERROR("Costmap is not initialized");
+    return 0;
+  }
+
+  // check if the costmap has an inflation layer
+  for(std::vector<boost::shared_ptr<costmap_2d::Layer> >::const_iterator layer = costmap_ros_->getLayeredCostmap()->getPlugins()->begin();
+      layer != costmap_ros_->getLayeredCostmap()->getPlugins()->end();
+      ++layer) {
+    boost::shared_ptr<costmap_2d::InflationLayer> inflation_layer = boost::dynamic_pointer_cast<costmap_2d::InflationLayer>(*layer);
+    if (!inflation_layer) continue;
+
+    result = costMapCostToSBPLCost(inflation_layer->computeCost(costmap_ros_->getLayeredCostmap()->getCircumscribedRadius() / costmap_ros_->getCostmap()->getResolution()));
+  }
+  return result;
+}
+
 bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
                                  const geometry_msgs::PoseStamped& goal,
                                  std::vector<geometry_msgs::PoseStamped>& plan){
   if(!initialized_){
     ROS_ERROR("Global planner is not initialized");
     return false;
+  }
+
+  bool do_init = false;
+  if (current_env_width_ != costmap_ros_->getCostmap()->getSizeInCellsX() ||
+      current_env_height_ != costmap_ros_->getCostmap()->getSizeInCellsY()) {
+    ROS_INFO("Costmap dimensions have changed from (%d x %d) to (%d x %d), reinitializing sbpl_lattice_planner.",
+             current_env_width_, current_env_height_,
+             costmap_ros_->getCostmap()->getSizeInCellsX(), costmap_ros_->getCostmap()->getSizeInCellsY());
+    do_init = true;
+  }
+  else if (footprint_ != costmap_ros_->getRobotFootprint()) {
+    ROS_INFO("Robot footprint has changed, reinitializing sbpl_lattice_planner.");
+    do_init = true;
+  }
+  else if (circumscribed_cost_ != computeCircumscribedCost()) {
+    ROS_INFO("Cost at circumscribed radius has changed, reinitializing sbpl_lattice_planner.");
+    do_init = true;
+  }
+
+  if (do_init) {
+    initialized_ = false;
+    delete planner_;
+    planner_ = NULL;
+    delete env_;
+    env_ = NULL;
+    initialize(name_, costmap_ros_);
   }
 
   plan.clear();
@@ -257,7 +318,7 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
       return false;
     }
   }
-  catch(SBPL_Exception e){
+  catch(SBPL_Exception *e){
     ROS_ERROR("SBPL encountered a fatal exception while setting the start state");
     return false;
   }
@@ -269,11 +330,11 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
       return false;
     }
   }
-  catch(SBPL_Exception e){
+  catch(SBPL_Exception *e){
     ROS_ERROR("SBPL encountered a fatal exception while setting the goal state");
     return false;
   }
-
+  
   int offOnCount = 0;
   int onOffCount = 0;
   int allCount = 0;
@@ -319,7 +380,7 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
     if(allCount > force_scratch_limit_)
       planner_->force_planning_from_scratch();
   }
-  catch(SBPL_Exception e){
+  catch(SBPL_Exception *e){
     ROS_ERROR("SBPL failed to update the costmap");
     return false;
   }
@@ -342,7 +403,7 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
       return false;
     }
   }
-  catch(SBPL_Exception e){
+  catch(SBPL_Exception *e){
     ROS_ERROR("SBPL encountered a fatal exception while planning");
     return false;
   }
@@ -353,14 +414,23 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
   try{
     env_->ConvertStateIDPathintoXYThetaPath(&solution_stateIDs, &sbpl_path);
   }
-  catch(SBPL_Exception e){
+  catch(SBPL_Exception *e){
     ROS_ERROR("SBPL encountered a fatal exception while reconstructing the path");
     return false;
   }
+  // if the plan has zero points, add a single point to make move_base happy
+  if( sbpl_path.size() == 0 ) {
+    EnvNAVXYTHETALAT3Dpt_t s(
+        start.pose.position.x - costmap_ros_->getCostmap()->getOriginX(),
+        start.pose.position.y - costmap_ros_->getCostmap()->getOriginY(),
+        theta_start);
+    sbpl_path.push_back(s);
+  }
+
   ROS_DEBUG("Plan has %d points.\n", (int)sbpl_path.size());
   ros::Time plan_time = ros::Time::now();
 
-  //create a message for the plan
+  //create a message for the plan 
   nav_msgs::Path gui_path;
   gui_path.poses.resize(sbpl_path.size());
   gui_path.header.frame_id = costmap_ros_->getGlobalFrameID();
@@ -374,7 +444,7 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
     pose.pose.position.y = sbpl_path[i].y + costmap_ros_->getCostmap()->getOriginY();
     pose.pose.position.z = start.pose.position.z;
 
-    tf::Quaternion temp;
+    tf2::Quaternion temp;
     temp.setRPY(0,0,sbpl_path[i].theta);
     pose.pose.orientation.x = temp.getX();
     pose.pose.orientation.y = temp.getY();
@@ -383,9 +453,7 @@ bool SBPLLatticePlanner::makePlan(const geometry_msgs::PoseStamped& start,
 
     plan.push_back(pose);
 
-    gui_path.poses[i].pose.position.x = plan[i].pose.position.x;
-    gui_path.poses[i].pose.position.y = plan[i].pose.position.y;
-    gui_path.poses[i].pose.position.z = plan[i].pose.position.z;
+    gui_path.poses[i] = plan[i];
   }
   plan_pub_.publish(gui_path);
   publishStats(solution_cost, sbpl_path.size(), start, goal);
